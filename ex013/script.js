a700908abf9a13fac56734159b80c9c2627f8828
Charles Braga <charlesqbraga.dev@gmail.com>
2023-06-08 21:03:31 -0300

    contador atualizado

--- ex013/script.js
+++ ex013/script.js
@@ -1,10 +1,10 @@
 function contar() {
-    let ini = document.getElementById ('txti')
-    let fim = document.getElementById ('txtf')
-    let passo = document.getElementById ('txtp')
-    let res = document.getElementById ('res')
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
-    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[Erro]')
     } else {
         res.innerHTML = 'Contando: '
@@ -13,15 +13,15 @@ function contar() {
         let p = Number(passo.value)
         if (i < f) {
             // Contagem crescente
-            for(let c = i; c <= f; c += p) {
+            for (let c = i; c <= f; c += p) {
                 res.innerHTML += `${c} \u{1F449}`
             }
         } else {
             // Contagem regressiva
-            for(let c = i; c >= f; c -= p) {
+            for (let c = i; c >= f; c -= p) {
                 res.innerHTML += ` ${c} \u{1F449}`
+            }
         }
-    }
-    res.innerHTML += `\u{1F3C1}`
+        res.innerHTML += `\u{1F3C1}`
     }
 }
