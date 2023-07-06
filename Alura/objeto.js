91ad7d5aa34470c2fa35e03d0097cda5634174aa
Charles Braga <charlesqbraga.dev@gmail.com>
2023-07-06 17:12:59 -0300

    Create objeto.js

---
+++ Alura/objeto.js
@@ -0,0 +1,13 @@
+const listaCPFs = [1111111, 2222222222, 333333333333333];
+
+const informacoesPessoa = ["nome", "josé", "idade", 32, "CPF", "111233"];
+
+console.log(informacoesPessoa[1])
+
+const objetoPessoa = {
+    idade: 32,
+    nome: "José", 
+    cpf: "111222333"
+}
+
+console.log(objetoPessoa.cpf)
